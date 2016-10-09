538a42128d135ccd7a6dc91c8aeb907d14831c1f
Francis <francisangeles3@hotmail.com>
2016-10-09 16:08:18 -0400

    Agregando las clases 'cartas' y 'mano'.

---
+++ Assets/Scripts/Modelos/Cartas.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Modelos 
+{
+	enum Palo
+	{
+		Trebol,
+		Diamante,
+		Corazon,
+		Espada
+	}
+
+	public class Cartas : MonoBehaviour {
+		Palo palo;
+
+		public Cartas(Palo palo)
+		{
+
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Modelos/Mano.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Modelos
+{
+
+	public class Mano : MonoBehaviour {
+		List<Cartas> cartas = new List<Cartas>();
+		int tamano;
+
+		public Mano()
+		{}
+
+		void AgregarCarta(Cartas carta)
+		{
+
+		}
+
+		void Limpiar()
+		{
+
+		}
+	}
+}
