2e834f21d1b94c3471be85de73efd6e26e89c35d
Francis <francisangeles3@hotmail.com>
2016-10-09 19:15:39 -0400

    Arreglado y con la clase cartas desarrollada

---
+++ Assets/Scripts/Modelos/Cartas.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Modelos
+{
+
+	enum Palos
+	{
+		Trebol,
+		Diamante,
+		Corazon,
+		Espada
+	}
+
+	public class Cartas {
+		private int numero_Carta;
+		private Palos palo;
+		string[] nombrePalo = {"clubs","diamonds","hearts","spades"};
+
+		public Cartas(Palos paloCarta, int numero)
+		{
+			numero_Carta = numero;
+			palo = paloCarta;
+		}
+
+		public int Numero_Carta {
+			get {numero_Carta;}
+			set {numero_Carta = value;}
+		}
+
+		public Palos Palo {
+			get {palo;}
+			set {palo = value;}
+		}
+
+		public override string ToString() {
+			return "card_" + nombrePalo[(int)palo] + "_" + numero_Carta;
+		}
+
+	}
+
+
+
+}
