101eea656296e9569195f018986778c9a98144ea
Francis <francisangeles3@hotmail.com>
2016-10-09 15:18:41 -0400

    Resolviendo

---
+++ Assets/Scripts/Modelos/Cartas.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+namespace Modelos
+{
+
+	enum Palo
+	{
+		Trebol,
+		Diamante,
+		Corazon,
+		Espada
+	}
+
+	public class Cartas : MonoBehaviour {
+		string nombrePalo;
+
+
+	}
+}
